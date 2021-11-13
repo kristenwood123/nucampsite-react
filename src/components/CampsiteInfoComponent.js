66884a01cf1ddb3cfdea76400a635d87c7e108f8
Kristen <kristenwood123@gmail.com>
2021-11-13 10:30:50 -0800

    added comments

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfoComponent extends Component {
+
   renderCampsite(campsite) {
     return (
       <div className="col-md-5 m-1">
@@ -16,16 +17,39 @@ class CampsiteInfoComponent extends Component {
     )
   }
 
+  renderComments(comments) {
+    if(comments) {
+      return(
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {(this.props.campsite.comments.map(comment => {
+            return (
+              <>
+              <p>{comment.text}</p>
+              <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+              </>
+            )
+          }))}
+        </div>
+      )
+    }
+    <div></div>
+  }
+
   render() {
     if(this.props.campsite) {
-      <div className="row"></div>
-    }
-    <div/>
+      return (
+      <div className="row">
+        {this.renderCampsite(this.props.campsite)}
+        {this.renderComments(this.props.campsite)}
+      </div>
+    )}
     return (
       <div>
         
       </div>
     );
+    
   }
 }
 
