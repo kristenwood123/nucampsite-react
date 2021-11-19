644b95a89a10c3a78d4efa853d2d7fc9f280771b
Kristen <kristenwood123@gmail.com>
2021-11-19 11:21:25 -0800

    adding single page app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import RenderCard from './RenderCard'
 
 function Home(props) {
     return (
-        <div className="container">
-            <h4>Home</h4>
+       <div className="container">
+            <div className="row">
+                <div className="col-md m-1">
+                    <RenderCard item={props.campsite} />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.partner} />
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,20 +7,30 @@ import Footer from './Footer'
 import Home from './Home'
 import Contact from './Contact'
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
   render() {
       const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         };
 
