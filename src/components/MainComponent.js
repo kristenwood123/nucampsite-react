2eed4c1d30640e3fe6f7ae96338d37e988a54ff0
Kristen <kristenwood123@gmail.com>
2021-11-19 11:10:46 -0800

    contact us

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,33 +1,65 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './Header'
 import Footer from './Footer'
 import Home from './Home'
+import Contact from './Contact'
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-const MainComponent = () => {
-  const [campsites, setCampsites] = useState(CAMPSITES)
-  const [selectedCampsite, setSelectedCampsite] = useState(null)
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+  render() {
+      const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        );
+        }
+      }
+
+export default Main
 
 
-  const onCampsiteSelect = (campsiteId) => {
-    setSelectedCampsite({selectedCampsite: campsiteId})
-  }
 
-  return (
-    <div>
-      <Header/>
-      <Switch>
-          <Route path='/home' component={<Home/>} />
-          <Route exact path='/directory' render={() => <Directory campsites={campsites} />} />
-          <Redirect to='/home'/>
-        {/* <CampsiteInfo campsite={campsites.filter(campsite => campsite.id === selectedCampsite[0])} /> */}
-      </Switch>
-      <Footer/>
-    </div>
-  )
-}
+  // const onCampsiteSelect = (campsiteId) => {
+  //   setSelectedCampsite({selectedCampsite: campsiteId})
+  // }
 
-export default MainComponent
+  // const HomePage = () => {
+  //           return (
+  //               <Home />
+  //           );
+  //       }
+  // return (
+  //   <div>
+  //     <Header/>
+  //     <Switch>
+  //         <Route path='/home' component={HomePage} />
+  //         <Route exact path='/directory' render={() => <Directory campsites={campsites} />} />
+  //         <Redirect to='/home'/>
+  //       <CampsiteInfo campsite={campsites.filter(campsite => campsite.id === selectedCampsite[0])} />
+  //     </Switch>
+  //     <Footer/>
+  //   </div>
