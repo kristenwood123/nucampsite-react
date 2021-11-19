9c8385b873b041c01fe8ae2ae37f45530e8acfcc
Kristen <kristenwood123@gmail.com>
2021-11-19 10:38:39 -0800

    updated app.css

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 
 // Components
 import Main from './components/MainComponent';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <Main/>
+            <div>
+                <Header />
+                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
+                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Footer />
             </div>
         );
     }
